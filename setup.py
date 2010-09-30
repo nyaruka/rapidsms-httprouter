bcf2970ddcc29240e1d2d7e617d8534723606625
Nic Pottier <nicpottier@gmail.com>
2010-09-30 10:44:04 +0200

    add setup.py and associated files so it can be installed via git

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rapidsms-httprouter',
+    version=__import__('rapidsms_httprouter').__version__,
+    license="BSD",
+
+    install_requires = [
+        "rapidsms",
+    ],
+
+    description='Provides a HTTP endpoints into the RapidSMS router, doing all handling in the HTTP thread.',
+    long_description=open('README.rst').read(),
+
+    author='Nicolas Pottier, Eric Newcomer',
+    author_email='code@nyaruka.com',
+
+    url='http://github.com/nyaruka/rapidsms-httprouter',
+    download_url='http://github.com/nyaruka/rapidsms-httprouter/downloads',
+
+    include_package_data=True,
+
+    packages=['rapidsms_httprouter'],
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
