c976b88f1039c2b354b33b4ca3ead108dea8df5f
David McCann <david.a.mccann@gmail.com>
2010-10-24 20:04:37 +0300

    auto_add_now actually overrrides dates being set, changed to defaulting to now()

--- rapidsms_httprouter/models.py
+++ rapidsms_httprouter/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 from rapidsms.models import Contact, Connection
@@ -24,7 +26,7 @@ class Message(models.Model):
     text       = models.TextField()
     direction  = models.CharField(max_length=1, choices=DIRECTION_CHOICES)
     status     = models.CharField(max_length=1, choices=STATUS_CHOICES)
-    date       = models.DateTimeField(auto_now_add=True)
+    date       = models.DateTimeField(default=datetime.datetime.now())
 
     in_response_to = models.ForeignKey('self', related_name='responses', null=True)
 
