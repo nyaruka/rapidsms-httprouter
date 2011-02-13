399b00947be800ff5b859fa7e560fef9562daec7
Nic Pottier <nicpottier@gmail.com>
2011-02-13 17:38:14 +0200

    require staff status to see the console

--- rapidsms_httprouter/urls.py
+++ rapidsms_httprouter/urls.py
@@ -3,11 +3,11 @@
 
 from django.conf.urls.defaults import *
 from .views import receive, outbox, delivered, console
-from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = patterns("",
    ("^router/receive", receive),
    ("^router/outbox", outbox),
    ("^router/delivered", delivered),
-   ("^router/console", login_required(console), {}, 'httprouter-console')
+   ("^router/console", staff_member_required(console), {}, 'httprouter-console')
 )
